8d36128eec5598c1f29aa8b1c6a57cdf4be84716
Mehmet Yücel Şirin <118905425+mhmtyclsrn@users.noreply.github.com>
2022-12-09 19:20:19 +0300

    Add files via upload

---
+++ src/harmonikOrt.java
@@ -0,0 +1,12 @@
+public class harmonikOrt {
+    public static void main(String[] args) {
+        double[] numbers = {1, 2, 3, 4, 5};
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += 1/(numbers[i]);
+
+        }
+
+        System.out.println(sum / numbers.length);
+    }
+}
